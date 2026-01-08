197681729ad9d35dbdc132bd6414dce3f52e432a
Sahanat7848 <nongtee7848@gmail.com>
2026-01-08 16:47:55 +0700

    ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ServerError } from './server-error/server-error';
+import { NotFound } from './not-found/not-found';
+import { Profile } from './profile/profile';
+import { Login } from './login/login';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: Home},
+    {path: "login", component: Login},
+    {path: "profile", component: Profile},
+    {path: "server-error", component: ServerError},
+    {path: "**", component: NotFound},
+];
