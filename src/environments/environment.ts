9667d43b40251bcd643f7a08b4a99342330b7869
Sahanat7848 <nongtee7848@gmail.com>
2026-02-12 10:19:05 +0700

    ใใใ

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    baseUrl: '/api'
+    baseUrl: '/api',
+    supabaseUrl: 'https://vocoupaakfbdkvkrvymq.supabase.co',
+    supabaseKey: 'sb_publishable_BT1B141HjZSAsLspjDnpWQ_1VHfn_'
 };
